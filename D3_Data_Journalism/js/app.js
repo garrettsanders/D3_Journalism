019d924d271cf85936434e1dfa83f54a6e9a483d
garrettsanders <gjsand74@gmail.com>
2020-05-27 13:03:08 -0700

    updated code

--- D3_Data_Journalism/js/app.js
+++ D3_Data_Journalism/js/app.js
@@ -1,1 +1,101 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// import data
+
+d3.csv("../data/data.csv").then(function(data) {
+    data.forEach(function(filteredData) {
+        filteredData.smokes;
+        filteredData.age;
+        filteredData.state;
+    });
+
+    var xLinearScale = d3.scaleLinear()
+      .domain([20, d3.max(data, d => d.smokes)])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.age)])
+      .range([height, 0]);
+
+    //create axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    //Append Axes to the graph
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis)
+
+    var circlesGroup = chartGroup.selectAll("circle")
+      .data(filteredData)
+      .enter()
+      .append("circle")
+      .attr("cx", d => xLinearScale(d.smokes))
+      .attr("cy", d => yLinearScale(d.age))
+      .attr("r", "15")
+      .attr("fill", "pink")
+      .attr("opacity", ".5");
+
+      //set up tool tip
+      var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function (d) {
+          return(`${d.state}<br>Smokes: ${smokes}<br>Age: ${d.age}`);
+
+      //create tooltip in graph
+      chartGroup.call(toolTip);
+
+      //event listeners
+      circlesGroup.on("click", function(data) {
+        toolTip.show(data, this);
+      })
+
+      //onmouseout event
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+// Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Number of Billboard 100 Hits");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("Hair Metal Band Hair Length (inches)");
+  }).catch(function(error) {
+    console.log(error);
+
+
+          
+        })
+
+});
