f888f23fdfda10d020599f7e47a323a669314a06
garrettsanders <gjsand74@gmail.com>
2020-05-29 15:34:38 -0700

    updated code

--- D3_Data_Journalism/js/app.js
+++ D3_Data_Journalism/js/app.js
@@ -59,17 +59,35 @@ d3.csv("../data/data.csv").then(function(HealthData) {
       .append("circle")
       .attr("cx", d => xLinearScale(d.poverty))
       .attr("cy", d => yLinearScale(d.healthcare))
-      .attr("r", "15")
+      .attr("r", 15)
       .attr("fill", "blue")
-      .attr("opacity", ".5");
+      .attr("opacity", ".5")
+      .attr("stroke", "black");
+
+      var circleLabels = chartGroup.selectAll(null).data(HealthData).enter().append("text");
+
+      circleLabels
+        .attr("x", function(d) {
+          return xLinearScale(d.poverty);
+        })
+        .attr("y", function(d) {
+          return yLinearScale(d.healthcare);
+        })
+        .text(function(d) {
+          return d.abbr
+        })
+        .attr("font-size", "10px")
+        .attr("text-anchor", "middle");
+
 
       //set up tool tip
       var toolTip = d3.tip()
-        .attr("class", "toolTip")
+        .attr("id", "toolTip")
         .offset([80, -60])
         .html(function(d) {
           return(`${d.state}<br>Poverty: ${d.poverty}%<br>Healthcare: ${d.healthcare}%`);
         });
+        
 
       //create tooltip in graph
       chartGroup.call(toolTip);
@@ -82,20 +100,23 @@ d3.csv("../data/data.csv").then(function(HealthData) {
       })
 
       //onmouseout event
+      .on("mouseover", function(data) {
+        toolTip.show(data, this);
+      })
       .on("mouseout", function(data, index) {
-        toolTip.hide(data, index);
+        toolTip.hide(data);
       });
 // Create axes labels
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left + 40)
-      .attr("x", 0 - (height / 2))
+      .attr("y", 0 - margin.left + 20)
+      .attr("x", 0 - (height / 1.5))
       .attr("dy", "1em")
       .attr("class", "axisText")
       .text("Lacks Healthcare (%)");
 
     chartGroup.append("text")
-      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 20})`)
       .attr("class", "axisText")
       .text("In Poverty (%)");
   }).catch(function(error) {
